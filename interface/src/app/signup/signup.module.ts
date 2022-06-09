c799758d47dc4d0c8339160a1c7d5ed605df1d76
sharm <reachmalhar@gmail.com>
2022-06-09 23:22:37 +0530

    Added Signup Page

--- interface/src/app/signup/signup.module.ts
+++ interface/src/app/signup/signup.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,12 +8,17 @@ import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SignupPageRoutingModule
+    SignupPageRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [SignupPage]
 })
